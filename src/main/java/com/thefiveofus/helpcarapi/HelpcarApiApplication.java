6d6469c86db611ba59922461f5d407632c0809dd
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2023-03-26 18:09:01 +0530

    initial commit

---
+++ src/main/java/com/thefiveofus/helpcarapi/HelpcarApiApplication.java
@@ -0,0 +1,13 @@
+package com.thefiveofus.helpcarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpcarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpcarApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thefiveofus/helpcarapi/HelpcarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thefiveofus.helpcarapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpcarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
