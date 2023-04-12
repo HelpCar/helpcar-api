a203e50d6e73fd055805e17f6387b9f15af79284
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2023-04-13 00:29:19 +0530

    Create user register logic

---
+++ src/main/java/com/thefiveofus/helpcarapi/service/UserService.java
@@ -0,0 +1,26 @@
+package com.thefiveofus.helpcarapi.service;
+
+import com.thefiveofus.helpcarapi.model.User;
+import com.thefiveofus.helpcarapi.repository.UserRepo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserService {
+
+    private final UserRepo userRepo;
+
+    public void registerUser(User user){
+        user = User.builder()
+                .fullName(user.getFullName())
+                .email(user.getEmail())
+                .build();
+
+        userRepo.save(user);
+        log.info("User {} is saved", user.getId());
+
+    }
+}
